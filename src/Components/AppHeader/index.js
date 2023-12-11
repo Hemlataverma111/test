1beca75acb57046bcc63def54a071fd6258954c8
Hemlataverma111 <131270183+Hemlataverma111@users.noreply.github.com>
2023-12-12 00:08:56 +0530

    first commit

---
+++ src/Components/AppHeader/index.js
@@ -0,0 +1,85 @@
+import { BellFilled, MailOutlined } from "@ant-design/icons";
+import { Badge, Drawer, Image, List, Space, Typography } from "antd";
+import { useEffect, useState } from "react";
+import { getComments, getOrders } from "../../API";
+
+function AppHeader() {
+  const [comments, setComments] = useState([]);
+  const [orders, setOrders] = useState([]);
+  const [commentsOpen, setCommentsOpen] = useState(false);
+  const [notificationsOpen, setNotificationsOpen] = useState(false);
+
+  useEffect(() => {
+    getComments().then((res) => {
+      setComments(res.comments);
+    });
+    getOrders().then((res) => {
+      setOrders(res.products);
+    });
+  }, []);
+
+  return (
+    <div className="AppHeader">
+      <Image
+        width={70}
+        src="https://th.bing.com/th/id/OIP.dzchswqTae9xH9_Kli4aHQHaFj?w=200&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7"
+      ></Image>
+      <Typography.Title> <span style={{fontWeight:"bold",fontSize:"20px",marginRight:"1000px"}}>Hotel Management Dashboard UI</span><br></br>
+        <span style={{fontWeight:"lighter",fontSize:"15px"}}> by</span><span style={{fontWeight:"lighter",fontSize:"15px",color:"red"}}> Dimo.ui</span></Typography.Title>
+      <Space>
+        <Badge count={comments.length} dot>
+          <MailOutlined
+            style={{ fontSize: 20 }}
+            onClick={() => {
+              setCommentsOpen(true);
+            }}
+          />
+        </Badge>
+        <Badge count={orders.length}>
+          <BellFilled
+            style={{ fontSize: 24 }}
+            onClick={() => {
+              setNotificationsOpen(true);
+            }}
+          />
+        </Badge>
+      </Space>
+      <Drawer
+        title="Comments"
+        open={commentsOpen}
+        onClose={() => {
+          setCommentsOpen(false);
+        }}
+        maskClosable
+      >
+        <List
+          dataSource={comments}
+          renderItem={(item) => {
+            return <List.Item>{item.body}</List.Item>;
+          }}
+        ></List>
+      </Drawer>
+      <Drawer
+        title="Notifications"
+        open={notificationsOpen}
+        onClose={() => {
+          setNotificationsOpen(false);
+        }}
+        maskClosable
+      >
+        <List
+          dataSource={orders}
+          renderItem={(item) => {
+            return (
+              <List.Item>
+                <Typography.Text strong>{item.title}</Typography.Text> has been
+                ordered!
+              </List.Item>
+            );
+          }}
+        ></List>
+      </Drawer>
+    </div>
+  );
+}
+export default AppHeader;
